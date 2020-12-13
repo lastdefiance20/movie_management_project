cad127cc7a77fa9ced349bfaae116de2cc0c4f1f
Jaeyoon Jung <lastdefiance20@gmail.com>
2020-12-13 16:58:21 +0900

    final update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 //영화 관리 프로그램 by 20201809 정재윤
 
@@ -76,7 +77,7 @@ void addMV(Node* head_ptr, void(*add)(Node* head, MV mv)) {
 
 	//scanf 연속사용으로 버퍼비우기 필요
 	printf("영화 이름 : ");
-	scanf("%[^\n]s", mv.name);
+	scanf("%[^\n]s", &mv.name);
 	//공백포함 X  -> 수정함, 이제 이름에는 공백포함 가능
 	//scanf("%s", &mv.name); 옛날버전
 	clearInputBuffer();
@@ -90,11 +91,11 @@ void addMV(Node* head_ptr, void(*add)(Node* head, MV mv)) {
 	clearInputBuffer();
 
 	printf("영화 장르 : ");
-	scanf("%s", mv.genre);
+	scanf("%s", &mv.genre);
 	clearInputBuffer();
 
 	printf("영화 배급사 : ");
-	scanf("%s", mv.film_dist);
+	scanf("%s", &mv.film_dist);
 	clearInputBuffer();
 
 	add(head_ptr, mv);
@@ -124,16 +125,16 @@ void deleteNode(Node* head_ptr, char* name) {
 
 //영화 삭제 함수 (노드 삭제 함수 호출(이름))
 void delMV(Node* head_ptr, void(*del)(Node* head, char* name)) {
-	char name[100]; //동적할당으로 바꿔보기
+	char name[100];
 	printf("지우고 싶은 영화의 이름을 입력하시오 : ");
 	scanf("%[^\n]s", name);
 	del(head_ptr, name);
 }
 
-//영화 검색 함수(검색 형식 함수 호출)
+//영화 검색 함수(검색 형식 함수 호출)(MV 출력 함수 호출)
 void searchMV(Node* head, int (*shouldprint)(const MV*, char* search), void(*print)(const MV*)) {
 	Node* cur = head->next;
-	char search[100]; //동적할당으로 바꿔보기
+	char search[100];
 	scanf("%[^\n]s", search);
 
 	printf("\n------검색 결과------\n");
@@ -161,6 +162,35 @@ int searchgenre(const MV* mv, char* genre) {
 int searchfilm_dist(const MV* mv, char* film_dist) {
 	if (strcmp(mv->film_dist, film_dist) == 0) return 1;
 	else return 0;
+}
+
+////영화 랜덤 선택 이후 영화 출력 함수 (MV 출력 함수 호출)
+void RandomPrint(Node* head, void(*print)(const MV*)) {
+	//만약 리스트가 비었을경우 리턴
+	if (head->next == NULL) return;
+
+	//연결 리스트 개수 서칭
+	Node* cur = head->next;
+	int movie_num = 0;
+	while (cur != NULL) {
+		movie_num++;
+		cur = cur->next;
+	}
+
+	//리스트 개수 내에서 랜덤으로 수를 하나 고름 - random_num
+	srand(time(NULL));
+	int random_num = rand() % movie_num;
+
+	//추천 영화 출력 - random_num 번째 리스트 출력
+	printf("\n------추천 영화------\n");
+	cur = head->next;
+	movie_num = 0;
+	while (cur != NULL) {
+		if (movie_num == random_num)
+			print(&cur->mv);
+		movie_num++;
+		cur = cur->next;
+	}
 }
 
 //movie.dat 파일을 읽어와 연결리스트를 생성하는 함수 (노드 추가 함수 호출)
@@ -208,11 +238,54 @@ void saveMV(Node* head) {
 	fclose(fp);
 }
 
+//sorting 함수
+void sortByRuntime(Node** head2dPtr) {
+	Node* head = *head2dPtr;
+
+	//만약 리스트가 비었을경우 리턴
+	if (head->next == NULL) return;
+
+	//새로운 연결리스트 생성
+	Node* head_new = (Node*)malloc(sizeof(Node));
+	head_new->next = NULL;
+
+	while (head->next) {
+		//find least member
+		Node* least_node = head->next;
+
+		Node* cur_node = least_node;
+		int cur_runtime = cur_node->mv.run_time;
+
+		while (cur_node->next) {
+			Node* next_node = cur_node->next;
+			int next_runtime = next_node->mv.run_time;
+
+			//만약 작을경우
+			if (next_runtime < cur_runtime) {
+				cur_runtime = next_runtime;
+				least_node = next_node;
+			}
+			cur_node = next_node;
+		}
+		//새로운 list에 least_node 추가
+		addNode(head_new, least_node->mv);
+
+		//기존 list에서 least_node 삭제
+		deleteNode(head, least_node->mv.name);
+	}
+	//기존 list는 비어있고 새로운 list는 정렬되어 꽉 찬 상태
+	free(head);
+
+	//연결리스트의 시작 head를 새로운 연결리스트로 교체
+	*head2dPtr = head_new;
+}
+
+
 //메인 함수, 주요 코드 포함 금지 -> 다 함수로 빼라!
 int main() {
 	//add node
-	Node head;
-	head.next = NULL;
+	Node* head = (Node*)malloc(sizeof(Node));
+	head->next = NULL;
 
 	int n;
 
@@ -226,7 +299,9 @@ int main() {
 		printf("4. 영화 검색\n");
 		printf("5. 저장된 영화 목록 불러오기\n");
 		printf("6. 현재 영화 목록 저장\n");
-		printf("7. 나가기\n");
+		printf("7. 랜덤 영화 추천\n");
+		printf("8. 영화 목록 러닝타임순 정렬\n");
+		printf("9. 나가기\n");
 		printf("====================\n");
 		printf("원하시는 메뉴 : ");
 		scanf("%d", &n);
@@ -234,17 +309,17 @@ int main() {
 
 		if (n == 1)
 		{
-			printList(&head, printMVinfo);
+			printList(head, printMVinfo);
 		}
 
 		else if (n == 2)
 		{
-			addMV(&head, addNode);
+			addMV(head, addNode);
 		}
 
 		else if (n == 3)
 		{
-			delMV(&head, deleteNode);
+			delMV(head, deleteNode);
 		}
 
 		else if (n == 4)
@@ -259,30 +334,40 @@ int main() {
 
 			if (n == 1) {
 				printf("영화 이름을 입력하시오 : ");
-				searchMV(&head, searchname, printMVinfo);
+				searchMV(head, searchname, printMVinfo);
 			}
 			else if (n == 2) {
 				printf("영화 장르를 입력하시오 : ");
-				searchMV(&head, searchgenre, printMVinfo);
+				searchMV(head, searchgenre, printMVinfo);
 			}
 			else if (n == 3) {
 				printf("영화 배급사를 입력하시오 : ");
-				searchMV(&head, searchfilm_dist, printMVinfo);
+				searchMV(head, searchfilm_dist, printMVinfo);
 			}
 			else printf("제대로 입력하시오\n");
 		}
 
 		else if (n == 5)
 		{
-			readMV(&head, addNode);
+			readMV(head, addNode);
 		}
 
 		else if (n == 6)
 		{
-			saveMV(&head);
+			saveMV(head);
 		}
 
 		else if (n == 7)
+		{
+			RandomPrint(head, printMVinfo);
+		}
+
+		else if (n == 8)
+		{
+			sortByRuntime(&head);
+		}
+
+		else if (n == 9)
 		{
 			break;
 		}
@@ -293,6 +378,7 @@ int main() {
 			n = 0;
 		}
 	}
-
+	////메모리 해제(운영체제에게 반납)
+	free(head);
 	return 0;
 }
